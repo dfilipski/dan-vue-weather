d8805a4c330de6a009f5716e4472eb08d14f9515
Dan Filipski <dfilipski@firemail.cc>
2025-03-16 16:22:59 -0500

    Implement fetching from weather API

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,26 +1,33 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { VBtn, VCard, VCardTitle, VCardText } from 'vuetify/components'
-import { getWeatherDataForLocation } from '../weatherApi.ts'
+import { VBtn, VTextField, VCard, VCardTitle, VCardText } from 'vuetify/components'
+import { getWeather } from '../weatherApi.ts'
 
-const parameters = {
-  "latitude": 35.802,
-  "longitude": -86.9114,
-  "hourly": "temperature_2m",
-  "timezone": "auto",
-  "wind_speed_unit": "mph",
-  "temperature_unit": "fahrenheit",
-  "precipitation_unit": "inch",
+let city = ''
+let weather = ref(await getWeather("Thompson's Station"))
+
+async function updateWeather() {
+  weather.value = await getWeather(city)
+  console.log(`Getting weather for ${city}...`)
+  city = ''
 }
 
-const weather = await getWeatherDataForLocation(parameters)
 </script>
 
 <template>
+  <div style="display: flex; flex-direction: row; gap: 10px; align-items: center;">
+    <v-text-field label="City:" v-model="city" placeholder="Thompson's Station" type="text"/>
+    <div>
+      <v-btn color="primary" @click="updateWeather">Get<br/>Weather</v-btn>
+    </div>
+  </div>
   <v-card class="mx-auto" max-width="400">
-    <v-card-title>Weather</v-card-title>
+
+    <v-card-title>Weather in {{ weather.name }}</v-card-title>
     <v-card-text>
-      The temperature is {{ weather.current.temperature }}.
+      The temperature is {{ weather.main.temp }}ºF.<br />
+      The humidity is {{ weather.main.humidity }}%.<br />
+      The description is {{ weather.weather[0].description }}. {{ city }}
     </v-card-text>
   </v-card>
 </template>
--- src/weatherApi.ts
+++ src/weatherApi.ts
@@ -1,14 +1,47 @@
-export interface WeatherParams {
-    latitude: number;
-    longitude: number;
-    hourly: string;
-    timezone: string;
-    wind_speed_unit: string;
-    temperature_unit: string;
-    precipitation_unit: string;
-}
+import axios from 'axios'
 
-export async function getWeatherDataForLocation(params: WeatherParams) {
-    return { current: { temperature: 20, weatherCode: 800, windSpeed: 5, windDirection: 180 } }
+export interface Weather {
+    main: {
+        temp: number,
+        humidity: number,
+        pressure: number,
+    };
+    weather: {
+        description: string,
+        icon: string,
+    }[];
+    name: string;
+    sys: {
+        country: string,
+    };
 }
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY
 
+
+export async function getWeather(city: string): Promise<Weather> {
+    if (!apiKey) {
+        throw new Error('API key not found');
+    }
+
+    const baseUrl = 'https://api.openweathermap.org/data/2.5/weather' // ?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await axios.get<Weather>(baseUrl, {
+            params: {
+                q: city,
+                appid: apiKey,
+                units: 'imperial',
+            }
+        });
+
+        console.log(`Got: {${response.data.name}, ${response.data.main.temp}ºF, ${response.data.main.humidity}%, ${response.data.weather[0].description}}`);
+        return response.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            throw new Error(`API Error: ${error.response?.data.message || error.message}`);
+        }
+        else {
+            throw new Error(`An unexpected error occurred`);
+        }
+    }
+}
