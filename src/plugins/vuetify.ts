e8e5b4b82e575653f924b90eabf8351727cda00b
Dan Filipski <dfilipski@firemail.cc>
2025-03-16 10:16:03 -0500

    Add material design font

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,11 +1,14 @@
 // src/plugins/vuetify.ts
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 import { createVuetify } from 'vuetify'
-import 'vuetify/styles' // Import vuetify styles
 
 // Create Vuetify Instance
 const vuetify = createVuetify({
     theme: {
         defaultTheme: 'dark',
+    },
+    icons: {
+        defaultSet: 'mdi',
     },
 })
 
