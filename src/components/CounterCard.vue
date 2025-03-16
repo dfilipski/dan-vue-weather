0d8282bbf42a3dfe8fcc2227b249757019761152
Dan Filipski <dfilipski@firemail.cc>
2025-03-16 10:16:19 -0500

    Add CounterCard component

---
+++ src/components/CounterCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { VBtn, VCard, VCardTitle, VCardText } from 'vuetify/components'
+
+const count = ref(0)
+</script>
+
+<template>
+  <v-card class="mx-auto" max-width="400">
+    <v-card-title>Counter Card</v-card-title>
+    <v-card-text>
+      The count is {{ count }}.
+    </v-card-text>
+    <v-btn color="primary" @click="count++">
+      Click Me!
+    </v-btn>
+  </v-card>
+</template>
+
+<style scoped>
+.mx-auto {
+  margin: 20px;
+}
+</style>
